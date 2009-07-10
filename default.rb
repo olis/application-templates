219309d02f942e9efb9dd020e5722523af8c31a0
Oliver Schemmel <os@yum.de>
2009-07-10 11:31:50 +0200

    Umstellung auf öffentliches Repository

--- default.rb
+++ default.rb
@@ -1,7 +1,7 @@
 
-load_template 'http://github.com/yum/application-templates/base.rb' #=> github url
-load_template 'http://github.com/yum/application-templates/_authenticated.rb' if yes?("Do you need authentication?")
-load_template 'http://github.com/yum/application-templates/_localized.rb' if yes?("Do you need internationalization?")
+load_template 'http://github.com/olis/application-templates/base.rb' #=> github url
+load_template 'http://github.com/olis/application-templates/_authenticated.rb' if yes?("Do you need authentication?")
+load_template 'http://github.com/olis/application-templates/_localized.rb' if yes?("Do you need internationalization?")
 
 # Success!
   puts "SUCCESS!"
