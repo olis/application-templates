0f5684c8e0198c2013e5813630c3bb4bbbe643f0
Oliver Schemmel <os@yum.de>
2009-07-10 10:47:14 +0200

    Benutzerinteraktion

--- default.rb
+++ default.rb
@@ -1,6 +1,6 @@
-load_template '/Users/olis/rails/templates/base.rb' #=> github url
-load_template '/Users/olis/rails/templates/_authenticated.rb'
-load_template '/Users/olis/rails/templates/_localized.rb'
+load_template '/Users/olis/rails/application-templates/base.rb' #=> github url
+load_template '/Users/olis/rails/application-templates/_authenticated.rb' if yes?("Do you need authentication?")
+load_template '/Users/olis/rails/application-templates/_localized.rb' if yes?("Do you need internationalization?")
 
 # Success!
   puts "SUCCESS!"
